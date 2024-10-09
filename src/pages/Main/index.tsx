0f5c6c52ea73e99bb07e721531793188cf8cc431
GabrielProjec <gabriel.acesso3@gmail.com>
2024-10-09 17:31:01 -0300

    FirstCommit

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from './styles'
+
+import { Sidebar } from '../../components/Sidebar'
+
+export default function Main() {
+  return (
+    <Container>
+      <Sidebar />
+
+      <h1>Main</h1>
+    </Container>
+  )
+}
