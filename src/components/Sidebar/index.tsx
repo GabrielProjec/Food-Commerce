55730ce372f18eb3e5b403ccee0997290d652d33
GabrielProjec <gabriel.acesso3@gmail.com>
2024-10-09 17:51:48 -0300

    iconSidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,11 +1,13 @@
 import { Container } from "./styles"
 
 import {ReactComponent as BurgerIcon} from '../../assets/burger.svg'
+import menuImg from '../../assets/menu.svg'
 
 export function Sidebar() {
     return (
         <Container>
-            <button>x
+            <button type="button">
+                <img src={menuImg} alt="Icon de Menu" />
             </button>
 
             <nav>
