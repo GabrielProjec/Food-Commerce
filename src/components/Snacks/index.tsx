01940d548f8fddbc8e686d2773ae32852572a0ca
GabrielProjec <gabriel.acesso3@gmail.com>
2024-10-16 15:43:39 -0300

    formatPrice

--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -1,5 +1,6 @@
 import { Container } from "./styles"
 import {FiPlus} from 'react-icons/fi'
+import { currentFormat } from "../../helpers/currentFormat"
 
 interface SnacksProps {
     snacks: any[]
@@ -8,19 +9,21 @@ interface SnacksProps {
 function Snacks({ snacks }: SnacksProps) {
     return (
         <Container>
-            {snacks.map((snack) => (
-                <div key={snack.id} className="snack">
-                    <h2>{snack.name}</h2>
-                    <img src={snack.image} alt={snack.name} />
-                    <p>{snack.description}</p>
-                    <div>
-                        <strong>{snack.price}</strong>
-                        <button type="button">
-                            <FiPlus />
-                        </button>
+            {snacks.map((snack) => {
+                return (
+                    <div key={snack.id} className="snack">
+                        <h2>{snack.name}</h2>
+                        <img src={snack.image} alt={snack.name} />
+                        <p>{snack.description}</p>
+                        <div>
+                            <strong>{currentFormat(snack.price)}</strong>
+                            <button type="button">
+                                <FiPlus />
+                            </button>
+                        </div>
                     </div>
-                </div>
-            ))}
+                )
+            })}
         </Container>
     )
 }
---
+++ src/helpers/currentFormat.ts
@@ -0,0 +1,6 @@
+export function currentFormat(current: number){
+    return new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    }).format(current)
+}
