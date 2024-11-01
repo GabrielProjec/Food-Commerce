7ea0d71e4f0c9be18c48e98a842b82253de3842e
GabrielProjec <gabriel.acesso3@gmail.com>
2024-11-01 13:35:01 -0300

    quantityCart

--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -1,25 +1,27 @@
 import { Container } from "./styles"
-import {FiPlus} from 'react-icons/fi'
+import { FiPlus } from 'react-icons/fi'
 import { currentFormat } from "../../helpers/currentFormat"
 import SkeletonSnacks from "../SkeletonSnacks"
 import { SnackData } from "../../interfaces/SnackData"
 import { useCart } from "../../hooks/useCart"
 
 interface SnacksProps {
-    snacks: SnackData[]
+  snacks: SnackData[]
 }
 
 function Snacks({ snacks }: SnacksProps) {
 
-  const {addSnackIntoCart} = useCart()
+  const { cart, addSnackIntoCart } = useCart()
 
-    return (
-        <Container>
-          {!snacks.length
+  return (
+    <Container>
+      {!snacks.length
         ? [1, 2, 3, 4].map((n) => <SkeletonSnacks key={n} />)
-        : snacks.map((snack) => (
+        : snacks.map((snack) => {
+          const snackExitent = cart.find((item) => item.snack === snack.snack && item.id === snack.id)
+          return (
             <div key={snack.id} className='snack'>
-              <span>1</span>
+              {snackExitent && <span>{snackExitent.quantity}</span>}
               <h2>{snack.name}</h2>
               <img src={snack.image} alt={snack.name} />
               <p>{snack.description}</p>
@@ -30,9 +32,10 @@ function Snacks({ snacks }: SnacksProps) {
                 </button>
               </div>
             </div>
-          ))}
-        </Container>
-    )
+          )
+        })}
+    </Container>
+  )
 }
 
 export default Snacks
